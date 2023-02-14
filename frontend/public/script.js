b92e73a1d4e09533fc3c0fb424c66a431f84d322
Valentin1289 <valibaiacof1289@gmail.com>
2023-02-14 13:21:57 +0200

    set loader

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,46 +1,43 @@
 let fetchedData;
 
 const getData = async () => {
-
-    // try {
-        const response = await fetch('/api/pizza/');
-        let data = await response.json();
-
-        fetchedData = data;
-        displayData()
-
-    //   } catch (error) {
-    //     if (error instanceof SyntaxError) {
-    //       // Unexpected token < in JSON
-    //       console.log('There was a SyntaxError', error);
-    //     } else {
-    //       console.log('There was an error', error);
-    //     }
-    //   }
-   
-}
+  try {
+  let image = document.createElement("img");
+  document.body.append(image);
+  image.src =
+    "https://mir-s3-cdn-cf.behance.net/project_modules/max_632/04de2e31234507.564a1d23645bf.gif";
+  const response = await fetch("/api/pizza/");
+  fetchedData = await response.json();
+  image.style.display = "none";
+  displayData();
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        console.log('There was a SyntaxError', error);
+      } else {
+        console.log('There was an error', error);
+      }
+    }
+};
 
 const displayData = () => {
-    // console.log(fetchedData);
 
-    const rootEl = document.getElementById("root");
+  const rootEl = document.getElementById("root");
 
-    const gridContainerEl = document.createElement("div");
-    gridContainerEl.className = "grid-container";
-    rootEl.appendChild(gridContainerEl);
+  const gridContainerEl = document.createElement("div");
+  gridContainerEl.className = "grid-container";
+  rootEl.appendChild(gridContainerEl);
 
-    fetchedData.types.forEach(element => {
-        const divEl = document.createElement("div");
-        divEl.innerHTML = element.name;
-        divEl.id = `${element.name}`;
-        divEl.className = "pizzaItemContainer";
-        gridContainerEl.appendChild(divEl);
-    });
+  fetchedData.types.forEach((element) => {
+    const divEl = document.createElement("div");
+    divEl.innerHTML = element.name;
+    divEl.id = `${element.name}`;
+    divEl.className = "pizzaItemContainer";
+    gridContainerEl.appendChild(divEl);
+  });
+};
 
-}
-
-const loadEvent = _ => {
-    getData();
+const loadEvent = (_) => {
+  getData();
 };
 
 window.addEventListener("load", loadEvent);
